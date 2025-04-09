e07b2b9a93116d0936969d2f0fb53be3113171a8
Michael Elliott - Anaconda <melliott@anaconda.com>
2025-04-09 09:33:36 +0100

    update revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="spider-crawler",
-    version="1.1.2",
+    version="1.1.3",
     description="A flexible web crawler for keyword searching and content extraction",
     author="Michael Elliott",
     author_email="melliott@anaconda.com",
